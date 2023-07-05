a90fa978166a1c3c900b12ce76995f80a8701bd4
Vibhav Pandey <104501131+vibhavpan@users.noreply.github.com>
2023-07-05 23:06:55 +0530

    Create Constants.kt

---
+++ Constants.kt
@@ -0,0 +1,10 @@
+package com.example.quizapppractise
+
+object Constants {
+
+    const val USER_NAME:String="user_name"
+    const val SCORE:String="user_score"
+    const val totalQuestions:Int=6
+
+    
+}
